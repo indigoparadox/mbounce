73e2a0ac2cca3036fdea19ec986e91f2ec8d2226
indigoparadox <indigo.repo@interfinitdynamics.info>
2019-07-16 20:31:23 -0400

    Reading input for acceleration.

--- src/bounce.py
+++ src/bounce.py
@@ -7,6 +7,11 @@ ACCEL_RIGHT = ( 1, 0)
 ACCEL_DOWN  = ( 0, 1)
 ACCEL_UP    = ( 0,-1)
 
+SPRITE_KEY_LEFT = 0
+SPRITE_KEY_RIGHT = 1
+SPRITE_KEY_LEFT_JUMP = 2
+SPRITE_KEY_RIGHT_JUMP = 3
+
 def accel( accel_factor, accel_dir, accel_max, accel_mult=1 ):
    i = 0
    while i < accel_mult:
@@ -40,11 +45,17 @@ def main():
    accel_factor = (0, 0)
    accel_rate = 1
    accel_max = (3, 5)
-   radius = 2
    screen_width = 640
    screen_height = 320
+   sprite_size = 23
+   sprite_margin = 1
+   sprite_xy = (28, 0)
+   sprite_frames = 0
+   sprite_frame_seq = 0
+   sprite_key = SPRITE_KEY_LEFT
+   sprite_frames_max = 30
    key_accel = (0, 0)
-   jump_max = 30
+   jump_max = 90
    jump_accel = 0
    jump_rate = -8
 
@@ -52,6 +63,21 @@ def main():
    screen = pygame.display.set_mode( (screen_width, screen_height) )
    running = True
    clock = pygame.time.Clock()
+
+   sprites = pygame.image.load( 'spritesheet.png' )
+   bgs = pygame.image.load( 'backgrounds.png' )
+
+   # Setup player sprites.
+   sprite_player_r1 = pygame.Surface( (23, 23) )
+   sprite_player_r1.blit( sprites, (0, 0), (
+         sprite_margin + (sprite_size * 28), 0,
+         sprite_size, sprite_size) )
+   sprite_player_r2 = pygame.Surface( (23, 23) )
+   sprite_player_r2.blit( sprites, (0, 0), (
+         sprite_margin + (sprite_size * 29), 0,
+         sprite_size, sprite_size) )
+   sprite_player_l1 = pygame.transform.flip( sprite_player_r1, True, False )
+   sprite_player_l2 = pygame.transform.flip( sprite_player_r2, True, False ) 
 
    while running:
 
@@ -103,7 +129,37 @@ def main():
 
       # Draw the current state of things.
       screen.fill( (0, 0, 0) )
-      pygame.draw.circle( screen, (255, 0, 0), dot_coords, radius )
+
+      # Advance all sprite animations.
+      sprite_frames += 1
+      if sprite_frames_max <= sprite_frames:
+         if 0 == sprite_frame_seq:
+            sprite_frame_seq = 1
+         else:
+            sprite_frame_seq = 0
+         sprite_frames = 0
+
+      if key_accel < 0:
+         if 0 == sprite_frame_seq:
+            screen.blit( sprite_player_l1, \
+               (dot_coords[0], dot_coords[1] - sprite_size), \
+               (0, 0, sprite_size, sprite_size) )
+         else:
+            screen.blit( sprite_player_l2, \
+               (dot_coords[0], dot_coords[1] - sprite_size), \
+               (0, 0, sprite_size, sprite_size) )
+         
+      else:
+         if 0 == sprite_frame_seq:
+            screen.blit( sprite_player_r1, \
+               (dot_coords[0], dot_coords[1] - sprite_size), \
+               (0, 0, sprite_size, sprite_size) )
+         else:
+            screen.blit( sprite_player_r2, \
+               (dot_coords[0], dot_coords[1] - sprite_size), \
+               (0, 0, sprite_size, sprite_size) )
+
+      #pygame.draw.circle( screen, (255, 0, 0), dot_coords, radius )
       font = pygame.font.SysFont( 'Sans', 14, False, False )
       stats = font.render( \
          '{}a_x: {}, a_y: {}, j: {}, x: {}, y: {}'.format( \
